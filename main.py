939b7fe43b47c8176a12b8b4382064a135c5ff3b
Henry Han <henryyy3@hotmail.com>
2018-03-22 17:28:01 +0800

    Added font, made welcome screen.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 1024, 768
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -13,5 +13,10 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
 
+    myfont = pygame.font.Font("helsinki.ttf", 60)
+
+    label = myfont.render("Welcome to SoleMates!", 1, (255, 255, 0))
     screen.fill(black)
+    screen.blit(label, (100, 100))
     pygame.display.flip()
+
