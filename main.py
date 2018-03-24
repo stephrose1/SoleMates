e35144958db5092e7e822ba560c4ac9443f911a2
Sam Kidman <samuel@netwealth.com.au>
2018-03-24 13:41:58 +1100

    add fps counter

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ size = width, height = 1024, 768
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+game_font = pygame.font.Font("helsinki.ttf", 60)
+system_font = pygame.font.SysFont("monospace", 20)
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    myfont = pygame.font.Font("helsinki.ttf", 60)
-
-    label = myfont.render("Welcome to SoleMates!", 1, (255, 255, 0))
+    fps_label = system_font.render('%.2f' % clock.get_fps(), True, (255, 0, 0))
     screen.fill(black)
-    screen.blit(label, (100, 100))
+    screen.blit(fps_label, (0, 0))
     pygame.display.flip()
 
+    # 30 FPS
+    clock.tick(30)
