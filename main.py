305fb77aaf0e04cad9e75841754948c5c0cddfa0
Sam Kidman <samuel@netwealth.com.au>
2018-03-24 15:54:15 +1100

    render player debug info on screen

--- main.py
+++ main.py
@@ -12,17 +12,20 @@ clock = pygame.time.Clock()
 player = Player(clock)
 
 game_font = pygame.font.Font("helsinki.ttf", 60)
-system_font = pygame.font.SysFont("monospace", 20)
+system_font = pygame.font.SysFont("monospace", 10)
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+
     fps_label = system_font.render('%.2f' % clock.get_fps(), True, (255, 0, 0))
+    player_debug_label = system_font.render(str(player), True, (255, 0, 0))
     player_sprite = player.render()
     screen.fill(black)
     screen.blit(fps_label, (0, 0))
+    screen.blit(player_debug_label, (0, 10))
     screen.blit(player_sprite, player.pos)
     pygame.display.flip()
 
--- player.py
+++ player.py
@@ -6,8 +6,11 @@ class Player:
     def __init__(self, clock):
         self.pos = (50, 50)
         self.vel = (0, 0)
+        self.dir = math.pi / 4
         self.clock = clock
         self.sprite = Player.create_sprite()
+        self.turning = 0
+        self.moving = 0
 
     def get_speed(self):
         (x, y) = self.vel
@@ -23,7 +26,35 @@ class Player:
             self.pos = (x_pos, y_pos)
 
     def render(self):
-        return self.sprite
+        return pygame.transform.rotate(self.sprite, math.degrees(self.dir))
+
+    def __repr__(self):
+        (x_pos, y_pos) = self.pos
+        (x_vel, y_vel) = self.vel
+        turning_dir = 'not turning'
+        movement_dir = 'not moving'
+        bearing = math.degrees(self.dir)
+
+        if self.turning < 0:
+            turning_dir = 'left'
+        elif self.turning > 0:
+            turning_dir = 'right'
+
+        if self.moving < 0:
+            movement_dir = 'backward'
+        if self.moving > 0:
+            movement_dir = 'forward'
+
+        return (
+            "position: ({x_pos:.2f}, {y_pos:.2f}), "
+            "velocity: ({x_vel:.2f}, {y_vel:.2f}), "
+            "bearing: {bearing:.2f}, "
+            "turning: {turning_dir}, "
+            "moving: {movement_dir}"
+        ).format(
+            x_pos=x_pos, y_pos=y_pos, x_vel=x_vel, y_vel=y_vel,
+            bearing=bearing, turning_dir=turning_dir, movement_dir=movement_dir
+        )
 
     @classmethod
     def create_sprite(cls):
