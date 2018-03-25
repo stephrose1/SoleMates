d9bcee508fbad5ab2416b4266d21505bd427d390
Sam Kidman <samuel@netwealth.com.au>
2018-03-25 12:21:18 +1100

    add comments to geometry library

--- point.py
+++ point.py
@@ -1,13 +1,17 @@
 from numpy import matrix
 from math import cos, sin
 
+"""2D point geometry library"""
+
 
 def rotate(point, angle):
+    """rotate a given point about the origin"""
     rot = matrix(
         ([cos(angle), -sin(angle)],
          [sin(angle), cos(angle)])
     )
 
+    # got this from wikipedia: https://en.wikipedia.org/wiki/Rotation_matrix
     (x, y) = point
     [[x], [y]] = (rot * matrix(([x], [y]))).tolist()
 
@@ -15,20 +19,21 @@ def rotate(point, angle):
 
 
 def translate(point, x, y):
+    """move (translate) a point by x and y"""
     (point_x, point_y) = point
 
     return point_x + x, point_y + y
 
 
 class _max:
+    """helper class to represent a MAX value (greater than anything)"""
     def __lt__(self, other): return False
-
     def __gt__(self, other): return True
 
 
 class _min:
+    """helper class to represent a MIN value (less than anything)"""
     def __lt__(self, other): return True
-
     def __gt__(self, other): return False
 
 
@@ -36,6 +41,7 @@ MAX, MIN = _max(), _min()
 
 
 def bbox(points):
+    """find the bounding box of a set of points"""
     min_x = MAX
     min_y = MAX
     max_x = MIN
