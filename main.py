429818911ceefac9f31d2eb1beeb7a07b24fca66
Sam Kidman <samuel@netwealth.com.au>
2018-03-25 12:16:35 +1100

    make code clearer and add comments

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-player = Player(clock)
+player = Player()
 
 game_font = pygame.font.Font("helsinki.ttf", 60)
 system_font = pygame.font.SysFont("monospace", 10)
@@ -18,23 +18,30 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        # send key events to the player
         if event.type == pygame.KEYDOWN:
             player.handle_keydown(event)
         if event.type == pygame.KEYUP:
             player.handle_keyup(event)
 
+    # render fps label
     fps_label = system_font.render('%.2f' % clock.get_fps(), True, (255, 0, 0))
+
+    # render player debug info (this calls __repr__ on the player)
     player_debug_label = system_font.render(str(player), True, (255, 0, 0))
 
+    # update and render the player
     player.update()
     player_sprite, player_pos = player.render()
 
+    # draw things on the screen
     screen.fill(black)
     screen.blit(fps_label, (0, 0))
     screen.blit(player_debug_label, (0, 10))
     screen.blit(player_sprite, player_pos)
 
+    # flip the buffer
     pygame.display.flip()
 
-    # 30 FPS
+    # limit the game to 30 fps
     clock.tick(30)
--- player.py
+++ player.py
@@ -10,37 +10,59 @@ BACK_KEY = 115
 
 
 class Player:
+    # coordinates of points of player polygon
     MODEL = [(10, math.sqrt(7500)), (-10, math.sqrt(7500)), (-50, 0), (50, 0)]
-    MODEL_CENTER = (0, math.sqrt(7500) / 2)  # center of rotation
+
+    # center of rotation
+    MODEL_CENTER = (0, math.sqrt(7500) / 2)
+
+    # how much the player turns per tick
     TURN_SPEED = math.pi / 25
+
+    # how much the player moves per tick
     MOVE_SPEED = 6
+
+    # color of the player
     COLOUR = (255, 0, 0)
 
-    def __init__(self, clock):
+    def __init__(self):
+        """Set up initial state of the player"""
+        # current position of the player
         self.pos = (50, 50)
+
+        # current velocity of the player, as a vector
         self.vel = (0, 0)
+
+        # bearing of the player
         self.dir = math.pi
-        self.clock = clock
-        self.sprite = Player.create_sprite()
+
+        # which direction the player is turning
+        # -1 : left
+        #  1 : right
+        #  0 : not turning
         self.turning = 0
+
+        # which direction the player is moving
+        # -1 : backward
+        #  1 : forward
+        #  0 : not moving
         self.moving = 0
 
-    def get_speed(self):
-        (x, y) = self.vel
-        return abs(math.sqrt(math.pow(x, 2) + math.pow(x, 2)))
+    def update_dir(self):
+        """update the players bearing"""
+        self.dir += self.turning * Player.TURN_SPEED
+        self.dir = math.fmod(self.dir, 2 * math.pi)
 
-    def compute_vel(self):
+    def update_vel(self):
+        """update the players velocity using the movement direction and bearing"""
         speed = self.moving * Player.MOVE_SPEED
         x_vel = -math.sin(self.dir) * speed
         y_vel = math.cos(self.dir) * speed
         self.vel = (x_vel, y_vel)
 
-    def update(self):
-        self.dir += self.turning * Player.TURN_SPEED
-        self.dir = math.fmod(self.dir, 2 * math.pi)
-        self.compute_vel()
-
-        if self.get_speed() > 0:
+    def update_pos(self):
+        """update the players position"""
+        if self.moving != 0:
             (x_pos, y_pos) = self.pos
             (x_vel, y_vel) = self.vel
             x_pos += x_vel
@@ -48,7 +70,14 @@ class Player:
 
             self.pos = (x_pos, y_pos)
 
+    def update(self):
+        """update the player bearing, velocity and position"""
+        self.update_dir()
+        self.update_vel()
+        self.update_pos()
+
     def render(self):
+        """render the player model and return the surface and position on screen where it should be rendered"""
         model = [rotate(p, self.dir) for p in Player.MODEL]
         center = rotate(Player.MODEL_CENTER, self.dir)
 
@@ -68,6 +97,7 @@ class Player:
         return sprite, render_pos
 
     def __repr__(self):
+        """string representation of the player for debugging"""
         (x_pos, y_pos) = self.pos
         (x_vel, y_vel) = self.vel
         turning_dir = 'not turning'
@@ -95,15 +125,8 @@ class Player:
             bearing=bearing, turning_dir=turning_dir, movement_dir=movement_dir
         )
 
-    @classmethod
-    def create_sprite(cls):
-        sprite = pygame.Surface((100, 100))
-        points = ((0, 0), (50, 100), (100, 0))
-        pygame.draw.polygon(sprite, (255, 0, 0), points)
-
-        return sprite
-
     def handle_keydown(self, event):
+        """handle when player presses a key"""
         if event.key == LEFT_KEY:
             self.turning = - 1
         elif event.key == RIGHT_KEY:
@@ -114,6 +137,7 @@ class Player:
             self.moving = -1
 
     def handle_keyup(self, event):
+        """handle when player releases a key"""
         if event.key == LEFT_KEY or event.key == RIGHT_KEY:
             self.turning = 0
         if event.key == FORWARD_KEY or event.key == BACK_KEY:
