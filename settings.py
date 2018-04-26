1a28884a82ec1f5d9c36a80d02d08fcc50ff38b3
stephrose1 <37209473+stephrose1@users.noreply.github.com>
2018-04-27 06:21:27 +1000

    level one model

---
+++ settings.py
@@ -0,0 +1,24 @@
+# define colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+
+#game options
+WIDTH = 1024
+HEIGHT = 768
+TITLE = 'My game'
+FPS = 60
+BGCOLOUR = DARKGREY
+
+# tiles
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+# player settings
+PLAYER_SPEED = 300
+PLAYER_IMG = 'socky.png'
