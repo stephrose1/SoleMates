4c6e1a3ddc13c906001f653a5168e321c96b21b9
stephrose1 <37209473+stephrose1@users.noreply.github.com>
2018-05-08 03:16:11 +1000

    sprite animation added

--- sprites.py
+++ sprites.py
@@ -3,15 +3,46 @@ from settings import *
 vec = pygame.math.Vector2
 
 
+class Spritesheet:
+    def __init__(self, filename):
+        self.spritesheet = pygame.image.load(filename).convert()
+
+    def get_image(self, x, y, width, height):
+        image = pygame.Surface((width, height))
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        return image
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = game.player_img
+
+        self.walking = False
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.neutral_frames[0]
+
         self.rect = self.image.get_rect()
         self.vel = vec(0, 0)
         self.pos = vec(x, y)
+
+    def load_images(self):
+        self.neutral_frames = [self.game.player_spritesheet.get_image(60, 0, 32, 32),
+                               self.game.player_spritesheet.get_image(60, 33, 32, 31)]
+        for frame in self.neutral_frames:
+            frame.set_colorkey(BLACK)
+        self.walk_frames_r = [self.game.player_spritesheet.get_image(0, 33, 59, 32),
+                              self.game.player_spritesheet.get_image(0, 66, 59, 32),
+                              self.game.player_spritesheet.get_image(0, 0, 59, 32),
+                              self.game.player_spritesheet.get_image(0, 66, 59, 32)]
+        for frame in self.walk_frames_r:
+            frame.set_colorkey(BLACK)
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r:
+            self.walk_frames_l.append(pygame.transform.flip(frame, True, False))
+
 
     def get_keys(self):
         self.vel = vec(0, 0)
@@ -48,6 +79,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.y = self.pos.y
 
     def update(self):
+        self.animate()
         self.get_keys()
         self.pos += self.vel * self.game.dt
         self.rect.x = self.pos.x
@@ -55,19 +87,69 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = self.pos.y
         self.collide_with_obstacles('y')
 
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if self.vel.x != 0:
+            self.walking = True
+        else:
+            self.walking = False
+
+        if self.walking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.walk_frames_l)
+                bottom = self.rect.bottom
+                if self.vel.x > 0:
+                    self.image = self.walk_frames_r[self.current_frame]
+                else:
+                    self.image = self.walk_frames_l[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+
+        if not self.walking:
+            if now - self.last_update > 600:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.neutral_frames)
+                bottom = self.rect.bottom
+                self.image = self.neutral_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+
 
 class Clothes(pygame.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.mobs
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = game.clothes_img
+
+        self.attacking = False
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.neutral_frames[0]
+
         self.rect = self.image.get_rect()
         self.pos = vec(x, y)
 
+    def load_images(self):
+        self.neutral_frames = [self.game.clothes_spritesheet.get_image(0, 0, 134, 86)]
+        for frame in self.neutral_frames:
+            frame.set_colorkey(BLACK)
+        self.attacking_frames = [self.game.clothes_spritesheet.get_image(0, 190, 213, 87),
+                                 self.game.clothes_spritesheet.get_image(163, 0, 137, 121),
+                                 self.game.clothes_spritesheet.get_image(0, 87, 162, 121)]
+
     def update(self):
+        self.animate()
         self.rect.x = self.pos.x
         self.rect.y = self.pos.y
+
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if not self.attacking:
+            if now - self.last_update > 200:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.neutral_frames)
 
 
 class Spider(pygame.sprite.Sprite):
@@ -100,12 +182,43 @@ class Robot(pygame.sprite.Sprite):
         self.groups = game.all_sprites
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = game.robot_img
+
+        self.attacking = True
+        self.current_frame = 0
+        self.last_update = 0
+        self.load_images()
+        self.image = self.frames[0]
+
         self.rect = self.image.get_rect()
         self.vel = vec(0, 0)
         self.pos = vec(x, y)
 
+    def load_images(self):
+        self.frames = [self.game.robot_spritesheet.get_image(248, 0, 123, 208),
+                       self.game.robot_spritesheet.get_image(0, 209, 123, 208),
+                       self.game.robot_spritesheet.get_image(124, 209, 123, 208),
+                       self.game.robot_spritesheet.get_image(0, 209, 123, 208),
+                       self.game.robot_spritesheet.get_image(248, 0, 123, 208),
+                       self.game.robot_spritesheet.get_image(0, 0, 123, 208),
+                       self.game.robot_spritesheet.get_image(124, 0, 123, 208),
+                       self.game.robot_spritesheet.get_image(0, 0, 123, 208)]
+
+        for frame in self.frames:
+            frame.set_colorkey(BLACK)
+
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if self.attacking:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.frames)
+                self.image = self.frames[self.current_frame]
+                self.rect = self.image.get_rect()
+
+
+
     def update(self):
+        self.animate()
         self.pos += self.vel * self.game.dt
         self.rect.x = self.pos.x
         self.rect.y = self.pos.y
